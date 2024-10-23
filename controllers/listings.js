0c5180dd5e5eeb15fcc42a53333dfdeaa42dd664
tntyousif <yousif02bh@gmail.com>
2024-10-23 13:55:06 +0300

    add rout post

--- controllers/listings.js
+++ controllers/listings.js
@@ -15,8 +15,15 @@ router.get('/', async (req, res) => {
         console.log(error);
     }
 })
+
 router.get('/new', (req, res) => {
     res.render('listings/new.ejs');
+});
+
+router.post('/',async (req, res) => {
+    req.body.owner = req.session.user._id;
+    await Listing.create(req.body);
+    res.redirect('/listings');
 });
 
 module.exports = router;
